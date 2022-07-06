ec9440ab2eb2144cb89cc4c1133670e3624dbb73
IfeoluwaAroboto <104203257+IfeoluwaAroboto@users.noreply.github.com>
2022-07-06 19:43:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const btn3 = document.querySelector(".btn-3")
 const btn4 = document.querySelector(".btn-4")
 const btn5 = document.querySelector(".btn-5")
 let submit = document.querySelector(".submit")
-const number = document.querySelector(".number")
+let number = document.querySelector(".number")
 const box2 = document.querySelector(".box2")
-const rate = document.querySelector(".rate")
 const box = document.querySelector(".box")
-
 
  btn1.addEventListener("click", function(){
     console.log("clicked")
@@ -37,9 +35,8 @@ btn5.addEventListener("click", function(){
  })
 
  submit.addEventListener("click", function(){
-   
- if (box.classList.contains("show-box2")){
-     box.classList.remove("show-box2")
+ if (box2.classList.contains("show-box2")){
+     box2.classList.remove("show-box2")
  }  
  else {
      box2.classList.add("show-box2")
@@ -50,6 +47,10 @@ btn5.addEventListener("click", function(){
 else {
     box.classList.add("hide-box")
 }
-  
-
+number = 0
+if (number === 0){
+    alert("OOPS, LOOKS LIKE YOU DID NOT PICK A NUMBER")
+    box.classList.remove("hide-box")
+    box2.classList.remove("show-box2")
+}
  })
